8a0fb853508d5dafd7c71da3e962c82e4ab8eab2
V J Niranjani <56225155+niranjani-vj@users.noreply.github.com>
2025-02-11 10:51:06 +0000

    css changes

--- src/components/ButtonTest.tsx
+++ src/components/ButtonTest.tsx
@@ -5,30 +5,32 @@ const ButtonTest = () => {
   const [selectedNo, setSelectedNo] = useState(false);
   const [sizeRange, setSizeRange] = useState(80);
   const [selectYes,setSelectYes] = useState(false)
+  const [question,setQuestion] = useState('Will you be my Valentaine?')
   return (
     <>
       <div className="main-div">
-        <h1>Will you be my Valentaine?</h1>
+        <h1>{question}</h1>
         <button
           type="button"
           className="btn btn-primary"
           style={selectedNo ? { padding: sizeRange, fontSize: sizeRange } : {}}
-          onClick={()=>{setSelectYes(true),setSizeRange(10)}}
+          onClick={()=>{setSelectYes(true);setSizeRange(10);setQuestion('Awhhh! Stop youu♥♥♥')}}
         >
           Yes
         </button>
         <button
           type="button"
-          className="btn btn-danger"
+          className="btn btn-danger no-btn"
           style={sizeRange>300 || sizeRange==10 ? {visibility:'hidden'}:{}}
           onClick={() => {
             setSelectedNo(true);
             setSizeRange(sizeRange + 100);
+            setQuestion('Wait! You want to be mine valentaine?')
           }}
         >
           No
         </button>
-        <img src={gif} alt="awh-gif" style={selectYes?{visibility:'visible',width:500,height:500}:{visibility:'hidden'}}/>
+        {selectYes && <img src={gif} alt="awh-gif" style={{ width: 500, height: 500 }} />}
       </div>
     </>
   );
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,9 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    allowedHosts: ["gsdmkq-5173.csb.app"], // Allow your host here
+  },
+});
